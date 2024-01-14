8c9c296fc250ab0bce2d04426775d5b94e6c9702
theo-thanhlam <phatlam.media.vis@gmail.com>
2024-01-14 16:52:46 -0500

    Revoke the API

--- utils/database.utils.js
+++ utils/database.utils.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const connectDb = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Connected")
-    } catch (error) {
-        console.error("Failed to connect to DB")
-        
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected");
+  } catch (error) {
+    console.error("Failed to connect to DB");
+    console.log(error);
+  }
+};
 
 module.exports = connectDb;
